7527f4c239e07f125118ea95526b404c6e71590b
turkpenbayev <mr.turkpenbaev1997@gmail.com>
2018-08-07 19:27:45 +0600

    updated database creat js

--- create_db.js
+++ create_db.js
@@ -9,13 +9,13 @@ var con = mysql.createConnection({
 });
 
 con.connect(function(err) {
-    // if (err) throw err;
-    // console.log("Connected!");
-    // var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255), number VARCHAR(255), email VARCHAR(255), work VARCHAR(255), status VARCHAR(255))";
-    // con.query(sql, function (err, result) {
-    //     if (err) throw err;
-    //     console.log("Table created");
-    // });
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255), number VARCHAR(255), email VARCHAR(255), work VARCHAR(255), status VARCHAR(255))";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
     var sql = "INSERT INTO customers (name, address, number, email, work, status) VALUES ?";
     var name, address, number, email, work, status, values;
     for (var i=0; i<7500; i++){  
