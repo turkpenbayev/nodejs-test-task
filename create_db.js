22c64e2ae3e78a8eb5b36fe306abeea06eb2e289
turkpenbayev <mr.turkpenbaev1997@gmail.com>
2018-08-07 18:47:53 +0600

    added mysql

---
+++ create_db.js
@@ -0,0 +1,40 @@
+var mysql = require('mysql');
+var randomstring = require("randomstring");
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123456b",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+    // if (err) throw err;
+    // console.log("Connected!");
+    // var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255), number VARCHAR(255), email VARCHAR(255), work VARCHAR(255), status VARCHAR(255))";
+    // con.query(sql, function (err, result) {
+    //     if (err) throw err;
+    //     console.log("Table created");
+    // });
+    var sql = "INSERT INTO customers (name, address, number, email, work, status) VALUES ?";
+    var name, address, number, email, work, status, values;
+    for (var i=0; i<7500; i++){  
+        name = randomstring.generate(4);
+        address = randomstring.generate(7);
+        number = randomstring.generate(8);
+        email = randomstring.generate(6);
+        work = randomstring.generate(4);
+        status = randomstring.generate(4);
+        values = [
+            [name, address, number, email, work, status],
+            [name, address, number, email, work, status]
+        ];
+        con.query(sql, [values], function (err, result) {
+            if (err) throw err;
+            console.log("Number of records inserted: " + (i+1));
+        });
+    }
+
+    
+
+  });
--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 var express = require('express');
+var mysql = require('mysql'); 
+var paginate = require('express-paginate');
 
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use(paginate.middleware(10, 50));
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "123456b",
+    database: "mydb"
+});
+function getResult(){
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM customers", function (err, result, fields) {
+            if (err) throw err;
+            return result;
+        });
+    });
+} 
+
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');    
 });
 
 app.get('/test', function (req, res) {
-    obj = {name: 'Bauyrzhan', age: 21, sex: 'M'};
+    obj = getResult();
     res.render('test', {test_content: 'Test page', data: obj});    
-});
-
-app.get('/:id/:name', function (req, res) {
-    res.send('id - '+req.params.id + ' name - '+req.params.name);
 });
 
 app.listen(3000, '127.0.0.1')
